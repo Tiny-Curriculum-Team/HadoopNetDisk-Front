b5da532e3994cb9ffeb749d0219978a8a581c7df
fflosmume <3152292651@qq.com>
2022-12-28 00:07:30 +0800

    左侧导航栏优化

---
+++ vue.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, './', dir)
+}
+
+module.exports = {
+    // 部署应用包时的基本 URL,用法和 webpack 本身的 output.publicPath 一致
+  publicPath: './',  
+  // 输出文件目录
+  outputDir: 'dist',  
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: true,  
+  // 是否使用包含运行时编译器的 Vue 构建版本
+  runtimeCompiler: false,  
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,  
+  // 生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)
+  integrity: false,  
+  // webpack相关配置
+  chainWebpack: config => {
+    // svg rule loader
+    const svgRule = config.module.rule('svg') // 找到svg-loader
+    svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+    svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+    svgRule // 添加svg新的loader处理
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+    // 修改images loader 添加svg处理
+    const imagesRule = config.module.rule('images')
+    //将 icons 文件夹排除, 不让 url-loader 处理该文件夹
+    imagesRule.exclude.add(resolve('src/assets/icons'))
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+  }
+}
